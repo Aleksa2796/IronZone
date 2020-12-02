755d1a550118d45a457b710b64dd0aa8040fbdff
Алекса Савкић <alexa.savkic@gmail.com>
2020-12-02 08:38:22 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IronZone.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Membership()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Articles()
+        {
+            ViewBag.Message = "Here you can see our articles!";
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IronZone.MvcApplication" Language="C#" %>
